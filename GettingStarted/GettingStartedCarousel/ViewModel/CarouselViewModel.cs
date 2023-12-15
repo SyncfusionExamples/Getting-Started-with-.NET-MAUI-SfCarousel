8feb9f545f0611b1689177b1ba800972fe0e1dcb
PrithisSf3973 <prithis.sf3973@gmail.com>
2023-12-15 18:14:17 +0530

    Updated the sample.

--- GettingStarted/GettingStartedCarousel/ViewModel/CarouselViewModel.cs
+++ GettingStarted/GettingStartedCarousel/ViewModel/CarouselViewModel.cs
@@ -1,4 +1,4 @@
-﻿using GettingStartedCarousel.Model;
+﻿using Syncfusion.Maui.Carousel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +11,14 @@ namespace GettingStartedCarousel.ViewModel
     {
         public CarouselViewModel()
         {
-            ImageCollection.Add(new CarouselModel("image1.png"));
-            ImageCollection.Add(new CarouselModel("image2.png"));
-            ImageCollection.Add(new CarouselModel("image3.png"));
-            ImageCollection.Add(new CarouselModel("image4.png"));
-            ImageCollection.Add(new CarouselModel("image5.png"));
+            ImageCollection.Add(new SfCarouselItem() { ImageName = "image1.png" });
+            ImageCollection.Add(new SfCarouselItem() { ImageName = "image2.png" });
+            ImageCollection.Add(new SfCarouselItem() { ImageName = "image3.png" });
+            ImageCollection.Add(new SfCarouselItem() { ImageName = "image4.png" });
+            ImageCollection.Add(new SfCarouselItem() { ImageName = "image5.png" });
         }
-        private List<CarouselModel> imageCollection = new List<CarouselModel>();
-        public List<CarouselModel> ImageCollection
+        private List<SfCarouselItem> imageCollection = new List<SfCarouselItem>();
+        public List<SfCarouselItem> ImageCollection
         {
             get { return imageCollection; }
             set { imageCollection = value; }
